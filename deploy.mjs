44f219c850f3284b43d23255c322cfcd16f4c019
Markus Tacker <Markus.Tacker@NordicSemi.no>
2020-10-27 23:36:07 +0100

    ci(auth0): allow delete

--- deploy.mjs
+++ deploy.mjs
@@ -5,7 +5,7 @@ const config = {
   AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
   AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
   AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
-  AUTH0_ALLOW_DELETE: false,
+  AUTH0_ALLOW_DELETE: true,
 };
 
 const main = async () => {
